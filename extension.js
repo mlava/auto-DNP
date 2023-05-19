910325faa55d8d281d1a44231ee24732e967f778
Mark Lavercombe <mlava@users.noreply.github.com>
2023-05-19 13:11:57 +1000

    Add files via upload

--- extension.js
+++ extension.js
@@ -1,5 +1,4 @@
 var adnpMode, templateUID;
-var hashChange = undefined;
 var monitorUID = undefined;
 var exAPI = undefined;
 var checkTomorrowInterval = 0;
@@ -96,22 +95,15 @@ export default {
         } else {
             extensionAPI.settings.panel.create(config);
         }
-
         var today = new Date();
         var todayUID = window.roamAlphaAPI.util.dateToPageUid(today);
         var page = window.roamAlphaAPI.q(` [:find ?e :where [?e :block/uid "${todayUID}"]]`);
         if (page.length < 1) { // no DNP for today yet
             monitorUID = todayUID;
             window.roamAlphaAPI.data.addPullWatch("[:create/time]", `[:block/uid "${monitorUID}"]`, pullFunction);
-        } else { // check tomorrow
-            checkTomorrow();
+        } else {
+            checkDNP(adnpMode, todayUID);
         }
-
-        try { if (checkTomorrowInterval > 0) clearInterval(checkTomorrowInterval) } catch (e) { }
-        checkTomorrowInterval = setInterval(async () => {
-            await checkTomorrow()
-        }, 3600000);
-        checkDNP(adnpMode);
 
         // onChange
         function setMode(evt) {
@@ -122,65 +114,37 @@ export default {
                 extensionAPI.settings.panel.create(config2);
             }
         }
-
-        // on hashchange
-        hashChange = async (e) => {
-            checkDNP(adnpMode);
-        };
-        window.addEventListener('hashchange', hashChange);
-
-
     },
     onunload: () => {
-        window.removeEventListener('hashchange', hashChange);
         window.roamAlphaAPI.data.removePullWatch("[:create/time]", `[:block/uid "${monitorUID}"]`, pullFunction);
     }
 }
 
 function pullFunction(before, after) {
     window.roamAlphaAPI.data.removePullWatch("[:create/time]", `[:block/uid "${monitorUID}"]`, pullFunction);
-    checkDNP(adnpMode);
+    checkDNP(adnpMode, monitorUID);
 }
 
-async function checkTomorrow() { // check if tomorrow's DNP exists, and manage pullWatches
-    const tomorrow = new Date();
-    tomorrow.setDate(tomorrow.getDate() + 1);
-    var tomorrowUID = window.roamAlphaAPI.util.dateToPageUid(tomorrow);
-    var page = window.roamAlphaAPI.q(` [:find ?e :where [?e :block/uid "${tomorrowUID}"]]`);
-    if (page.length < 1) { // no uid for tomorrow yet
-        window.roamAlphaAPI.data.addPullWatch("[:create/time]", `[:block/uid "${tomorrowUID}"]`, pullFunction);
-        monitorUID = tomorrowUID;
-    } else { // removePullWatch as page exists
-        window.roamAlphaAPI.data.removePullWatch("[:create/time]", `[:block/uid "${tomorrowUID}"]`, pullFunction);
-    }
-}
-
-async function checkDNP(adnpMode) {
-    var pageUid = await window.roamAlphaAPI.ui.mainWindow.getOpenPageOrBlockUid();
-
-    if (!pageUid) {
-        var uri = window.location.href;
-        const regex = /^https:\/\/(relemma-.+)?roamresearch.com\/(#\/(app|offline)\/\w+)?$/; //today's DNP
-        let logPage = document.getElementById("rm-log-container");
-        if (uri.match(regex) || logPage) {
-            var today = new Date();
-            var dd = String(today.getDate()).padStart(2, '0');
-            var mm = String(today.getMonth() + 1).padStart(2, '0');
-            var yyyy = today.getFullYear();
-            pageUid = mm + '-' + dd + '-' + yyyy;
-        }
-    }
+async function checkDNP(adnpMode, pageUid) {
     var pageTitle = window.roamAlphaAPI.q(`[:find (pull ?p [:node/title]) :where [?p :block/uid "${pageUid}"]]`)?.[0]?.[0]?.title || "";
-
-    var currentDate = new Date();
-    var currentMonth = (currentDate.getMonth() + 1).toString();
-    var currentDay = currentDate.getDate().toString();
-    var currentYear = currentDate.getFullYear().toString();
-    currentDate = currentMonth.padStart(2, "0") + "-" + currentDay.padStart(2, "0") + "-" + currentYear;
     var d = new Date();
-    var dayOfWeek = d.getDay();    
+    var dayOfWeek = d.getDay();
 
-    if (adnpMode == "Daily") {
+    if (adnpMode == "Weekday/Weekend") {
+        if (dayOfWeek == 0) { // Sunday
+            if (exAPI.extensionAPI.settings.get("adnp-weekend")) {
+                templateUID = exAPI.extensionAPI.settings.get("adnp-weekend");
+            }
+        } else if (dayOfWeek == 6) { // Saturday
+            if (exAPI.extensionAPI.settings.get("adnp-weekend")) {
+                templateUID = exAPI.extensionAPI.settings.get("adnp-weekend");
+            }
+        } else { // weekdays
+            if (exAPI.extensionAPI.settings.get("adnp-weekday")) {
+                templateUID = exAPI.extensionAPI.settings.get("adnp-weekday");
+            }
+        }
+    } else {
         if (dayOfWeek == 0) {
             if (exAPI.extensionAPI.settings.get("adnp-Sun")) {
                 templateUID = exAPI.extensionAPI.settings.get("adnp-Sun");
@@ -210,68 +174,29 @@ async function checkDNP(adnpMode) {
                 templateUID = exAPI.extensionAPI.settings.get("adnp-Sat");
             }
         }
-    } else if (adnpMode == "Weekday/Weekend") {
-        if (dayOfWeek == 0) { // Sunday
-            if (exAPI.extensionAPI.settings.get("adnp-weekend")) {
-                templateUID = exAPI.extensionAPI.settings.get("adnp-weekend");
-            }
-        } else if (dayOfWeek == 6) { // Saturday
-            if (exAPI.extensionAPI.settings.get("adnp-weekend")) {
-                templateUID = exAPI.extensionAPI.settings.get("adnp-weekend");
-            }
-        } else { // weekdays
-            if (exAPI.extensionAPI.settings.get("adnp-weekday")) {
-                templateUID = exAPI.extensionAPI.settings.get("adnp-weekday");
-            }
-        }
     }
 
-    if (pageUid === currentDate) { // we need to paste the template as this is the right page
-        if (templateUID == undefined) {
-            alert("Make sure to set the template block reference in Roam Depot settings!");
-        } else {
-            templateUID = templateUID.replace('((', '');
-            templateUID = templateUID.replace('))', '');
-            checkPage(pageUid, pageTitle, templateUID);
-        }
-    } else { // check the sidebar to make sure today's DNP isn't open there
-        var RSwindows = await window.roamAlphaAPI.ui.rightSidebar.getWindows();
-        if (RSwindows) {
-            for (var i = 0; i < RSwindows.length; i++) {
-                if (RSwindows[i]['page-uid'] == currentDate) {
-                    pageUid = RSwindows[i]['page-uid'].toString();
-                }
+    if (templateUID == undefined) {
+        alert("Make sure to set your template block references in Roam Depot settings!");
+    } else {
+        templateUID = templateUID.replace('((', '');
+        templateUID = templateUID.replace('))', '');
+
+        let query = `[:find ?block_string :where [?p :node/title "${pageTitle}"] [?p :block/children ?c] [?c :block/string ?block_string]]`;
+        let results = window.roamAlphaAPI.q(query);
+
+        var parentBlockTextMatch = false;
+        let tree = getTreeByBlockUid(templateUID);
+        var parentBlockText = tree.children[0].text;
+        for (var i = 0; i < results.length; i++) {
+            if (results[i][0] == parentBlockText) {
+                parentBlockTextMatch = true;
             }
         }
-        if (templateUID == undefined) {
-            alert("Make sure to set the template block reference in Roam Depot settings!");
-        } else {
-            templateUID = templateUID.replace('((', '');
-            templateUID = templateUID.replace('))', '');
-            checkPage(pageUid, pageTitle, templateUID);
+        if (parentBlockTextMatch == false) {
+            await printTree(tree, pageUid);
+            setTomorrow(); // now that we've printed today's template, let's monitor for tomorrow
         }
-    }
-}
-
-async function checkPage(pageUid, pageTitle, templateUID) {
-    let query = `[:find ?block_string
-                :where
-                [?p :node/title "${pageTitle}"]
-                [?p :block/children ?c]
-                [?c :block/string ?block_string]]`;
-    let results = window.roamAlphaAPI.q(query);
-
-    var parentBlockTextMatch = false;
-    let tree = getTreeByBlockUid(templateUID);
-    var parentBlockText = tree.children[0].text;
-    for (var i = 0; i < results.length; i++) {
-        if (results[i][0] == parentBlockText) {
-            parentBlockTextMatch = true;
-        }
-    }
-    if (parentBlockTextMatch == false) {
-        await printTree(tree, pageUid);
-        checkTomorrow(); // now that we've printed today's template, let's see if we need to create a pullWatch for tomorrow
     }
 }
 
@@ -293,6 +218,25 @@ async function printTree(tree, pageUid) {
             }
         }
     }
+}
+
+async function setTomorrow() {
+    let today = new Date();
+    let millisToMidnight = new Date().setHours(24,0,0,0) - today;
+    var tomorrow = new Date(today);
+    tomorrow.setDate(tomorrow.getDate() + 1);
+    var tomorrowUID = window.roamAlphaAPI.util.dateToPageUid(tomorrow);
+
+    try { if (checkTomorrowInterval > 0) clearInterval(checkTomorrowInterval) } catch (e) { }
+    checkTomorrowInterval = setInterval(async () => {
+        var page = window.roamAlphaAPI.q(` [:find ?e :where [?e :block/uid "${tomorrowUID}"]]`);
+        if (page.length < 1) { // no DNP for tomorrow yet
+            monitorUID = tomorrowUID;
+            window.roamAlphaAPI.data.addPullWatch("[:create/time]", `[:block/uid "${monitorUID}"]`, pullFunction);
+        } else {
+            checkDNP(adnpMode, tomorrowUID);
+        }
+    }, millisToMidnight);
 }
 
 // I think these two helper functions came from either TFTHacker and Roam42, or possibly David Vargas at roamjs.com
